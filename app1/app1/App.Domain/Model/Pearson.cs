cb232fe3470af241871bd2cb1c1244fbea9ecc88
ohroggs <lorenzo.mari.11-1@accademiadigitaleliguria.it>
2023-02-27 12:39:51 +0100

    bah

---
+++ app1/app1/App.Domain/Model/Pearson.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Model
+{
+    internal class Pearson
+    {
+        int ID { get; set; }
+        string Name { get; set; }
+        string Surname { get; set; }
+        DateOnly Birthdate { get; set; }
+
+        [MaxLength(16)]
+        string Cf { get; set; }
+
+        string Email { get; set; }
+
+        int Iduser { get; set; }
+
+        [ForeignKey(nameof(Iduser))]
+        public virtual User User { get; set; }
+    }
+}
---
+++ app1/app1/App.Domain/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Model
+{
+    internal class User
+    {
+        int ID { get; set; }
+        string Nickname { get; set; }
+        string Hash { get; set; }
+        string Salt { get; set; }
+    }
+}
